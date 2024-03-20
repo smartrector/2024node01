812fe0a3471e51b1968f6cdbf1a07bbb775c91ac
smartrector <smartrector@gmail.com>
2024-03-20 12:31:41 +0900

    router

---
+++ src/routes/userRouter.js
@@ -0,0 +1,79 @@
+const {Router} = require("express");
+const {default: mongoose} = require("mongoose");
+const {User} = require("../models/User");
+const userRouter = Router();
+
+userRouter.get("/", async function (req, res) {
+  try {
+    const users = await User.find({});
+    return res.send({users});
+  } catch (error) {
+    return res.status(500).send({error: error.message});
+  }
+});
+
+// userRouter.get("/user/:userId",async function(req,res){
+//     try {
+
+//     } catch (error) {
+//         return res.status(500).send({error: error.message});
+//     }
+// })
+
+userRouter.get("/:userId", async function (req, res) {
+  try {
+    const {userId} = req.params;
+    const user = await User.findOne({_id: userId});
+    return res.send({user});
+  } catch (error) {
+    return res.status(500).send({error: error.message});
+  }
+});
+
+userRouter.post("/", async function (req, res) {
+  try {
+    const user = new User(req.body);
+    await user.save();
+    res.send(user);
+  } catch (error) {
+    return res.status(500).send({error: error.message});
+  }
+});
+// {
+//     "username":"hanyong5",
+//     "name":{
+//       "first":"han",
+//       "last":"sung yong"
+//      },
+//         "age":40,
+//       "email":"winoz@naver.com"
+//   }
+
+userRouter.delete("/:userId", async function (req, res) {
+  try {
+    const {userId} = req.params;
+    const user = await User.findByIdAndDelete({_id: userId});
+    return res.send({user});
+  } catch (error) {
+    return res.status(500).send({error: error.message});
+  }
+});
+
+userRouter.put("/:userId", async function (req, res) {
+  try {
+    const {age, email} = req.body;
+    const {userId} = req.params;
+    const user = await User.findByIdAndUpdate(
+      userId,
+      {
+        $set: {age, email},
+      },
+      {new: true}
+    );
+    return res.send({user});
+  } catch (error) {
+    return res.status(500).send({error: error.message});
+  }
+});
+
+module.exports = {userRouter};
--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const {default: mongoose} = require("mongoose");
 const app = express();
 const {User} = require("./models/User");
 const dotenv = require("dotenv");
+const {userRouter} = require("./routes/userRouter");
 
 dotenv.config();
 
@@ -13,61 +14,7 @@ const server = async function () {
 
     app.use(express.json());
 
-    app.get("/user", async function (req, res) {
-      try {
-        const users = await User.find({});
-        return res.send({users});
-      } catch (error) {
-        return res.status(500).send({error: error.message});
-      }
-    });
-
-    // app.get("/user/:userId",async function(req,res){
-    //     try {
-
-    //     } catch (error) {
-    //         return res.status(500).send({error: error.message});
-    //     }
-    // })
-
-    app.get("/user/:userId", async function (req, res) {
-      try {
-        const {userId} = req.params;
-        const user = await User.findOne({_id: userId});
-        return res.send({user});
-      } catch (error) {
-        return res.status(500).send({error: error.message});
-      }
-    });
-
-    app.post("/user", async function (req, res) {
-      try {
-        const user = new User(req.body);
-        await user.save();
-        res.send(user);
-      } catch (error) {
-        return res.status(500).send({error: error.message});
-      }
-    });
-    // {
-    //     "username":"hanyong5",
-    //     "name":{
-    //       "first":"han",
-    //       "last":"sung yong"
-    //      },
-    //         "age":40,
-    //       "email":"winoz@naver.com"
-    //   }
-
-    app.delete("/user/:userId", async function (req, res) {
-      try {
-        const {userId} = req.params;
-        const user = await User.findByIdAndDelete({_id: userId});
-        return res.send({user});
-      } catch (error) {
-        return res.status(500).send({error: error.message});
-      }
-    });
+    app.use("/user", userRouter);
 
     app.listen(3000);
   } catch (error) {
