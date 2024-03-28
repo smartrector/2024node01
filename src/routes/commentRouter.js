542e8c0a69b5d34aa3765f79fdb218d00fc95a65
smartrector <smartrector@gmail.com>
2024-03-28 14:26:32 +0900

    up

--- src/routes/commentRouter.js
+++ src/routes/commentRouter.js
@@ -12,15 +12,35 @@ commentRouter.post("/", async function (req, res) {
     const {blogId} = req.params;
     const {content, userId} = req.body;
 
-    const blog = await Blog.findOne({_id: blogId});
-    const user = await User.findOne({_id: userId});
+    const [blog, user] = await Promise.all([
+      Blog.findOne({_id: blogId}),
+      User.findOne({_id: userId}),
+    ]);
+
+    // const blog = await Blog.findOne({_id: blogId});
+    // const user = await User.findOne({_id: userId});
+
     if (!blog || !user) {
       return res
         .status(400)
         .send({err: "blog, user를 찾을수 없네요", blogId, userId, content});
     }
 
-    return res.send({blogId, userId, content});
+    const comment = new Comment({content, user, blog});
+    await comment.save();
+
+    return res.send({comment});
+    // return res.send({blogId, userId, content})
+  } catch (error) {
+    return res.status(500).send({error: error.message});
+  }
+});
+
+commentRouter.get("/", async function (req, res) {
+  try {
+    const {blogId} = req.params;
+    const comments = await Comment.find({blog: blogId});
+    return res.send({comments});
   } catch (error) {
     return res.status(500).send({error: error.message});
   }
